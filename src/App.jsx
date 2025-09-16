891edceeb7a599a08785f3dddc00f1b33ae592ba
Andrei Shilov <artmove3@gmail.com>
2025-09-16 10:07:55 +0700

    set project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+function App() {
+	return (
+		<Div>
+			<div>1234</div>
+			<i className="fa fa-camera-retro"></i>
+		</Div>
+	);
+}
 
 export default App;
